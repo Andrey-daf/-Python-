362bff1d3fdca03d3b7d739a6275d185c4075901
Andrey-daf <144468440+Andrey-daf@users.noreply.github.com>
2024-10-21 22:34:10 +0300

    Update Task 1.py

--- 1 лабараторная/Task 1.py
+++ 1 лабараторная/Task 1.py
@@ -2,7 +2,7 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 valid_numbers = [num for num in numbers if num is not None]
 average = sum(valid_numbers) / len(valid_numbers)
-average = -15.65
+
 
 numbers = [average if num is None else num for num in numbers]
 
