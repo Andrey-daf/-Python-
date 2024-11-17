c0afc99b1daf7cd73cb850fdbaf6be92bee35eff
Andrey-daf <144468440+Andrey-daf@users.noreply.github.com>
2024-11-17 23:06:48 +0300

    Update Сумма.py

--- финал/Сумма.py
+++ финал/Сумма.py
@@ -1,28 +1,16 @@
-# TODO импортировать необходимые молули
-import csv
+# TODO решите задачу
 import json
 
-INPUT_FILENAME = "input.csv"
-OUTPUT_FILENAME = "output.json"
+def task() -> float:
+    # Открываем файл JSON
+    with open('input.json', 'r') as file:
+        data = json.load(file)
+
+    # Вычисляем сумму произведений "score" * "weight"
+    total = sum(item["score"] * item["weight"] for item in data)
+
+    # Округляем до 3 знаков после запятой
+    return round(total, 3)
 
 
-def task() -> None:
-    ...  # TODO считать содержимое csv файла
-
-    with open(INPUT_FILENAME, mode='r', encoding='utf-8') as csv_file:
-        reader = csv.DictReader(csv_file)
-
-        data = [row for row in reader]
-
-    ...  # TODO Сериализовать в файл с отступами равными 4
-    with open(OUTPUT_FILENAME, mode='w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, indent=4, ensure_ascii=False)
-
-
-if __name__ == '__main__':
-    # Нужно для проверки
-    task()
-
-    with open(OUTPUT_FILENAME) as output_f:
-        for line in output_f:
-            print(line, end="")
+print(task())
